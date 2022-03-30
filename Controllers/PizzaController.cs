6548d344fa2cbade4dd120d3ca27255f8f1e5537
FRANCO DAMIAN <46647617@ort>
2022-03-30 09:47:05 -0300

    Clase 2

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using Pizzas.API.Models;
+using Pizzas.API.Helpers;
+using Pizzas.API.Utils;
+using Pizzas.API.Services;
+
+namespace Pizzas.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PizzaController{
+
+        [HttpGet]
+        public IActionResult GetAll(){
+            return Ok(PizzasServices.GetAll()); 
+        }
+
+        [HttpGet("{id}")] 
+        public IActionResult GetById (int id) {
+            return Ok(PizzasServices.getById(id));
+        }
+        
+        [HttpPost]
+        public IActionResult Create (Pizza pizza) {
+            return Ok(PizzasServices.Create(pizza));
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Pizza pizza) {
+           return Ok(PizzasServices.Update(id, pizza));
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteById(int id) {
+           return Ok(PizzasServices.DeleteById(id));
+        }  
+    }
+}
---
+++ Services/PizzasServices.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+
+using Pizzas.API.Models;
+using Pizzas.API.Helpers;
+using Pizzas.API.Utils;
+using Pizzas.API.Services;
+
+namespace Pizzas.API.PizzasServices{
+    public static class PizzasServices{
+
+        public static List <Pizza> GetAll(){
+            List<Pizza> pizzas = null;
+            String query ="SELECT * FROM Pizzas";
+            
+            using (SqlConnection db = ){
+                pizzas = db.Query<Pizza>(query).ToList();
+            }
+            return pizzas;
+        }
+
+        
+        public static Pizza GetById(int idPizza){
+            Pizza pizza = null;
+            String query ="SELECT * FROM Pizzas WHERE Id = @id";
+            
+            using (SqlConnection db = ){
+                pizza = db.QueryFirstOrDefault<Pizza>(query, new {id = idPizza});
+            }
+            return pizza;
+        }
+
+
+        public static int Create(Pizza pizza){
+            String query = "INSERT INTO Pizzas (Nombre, LibreGluten, Importe, Descripcion) VALUES (@nombre, @libreGluten, @importe, @descripcion)";
+            int RegistrosCreados = 0;
+            
+            using (SqlConnection db = ){
+                RegistrosCreador = db.Execute(query, new{nombre = pizza.nombre, libreGluten = pizza.LibreGluten, importe = pizza.Importe, descripcion = pizza.Descripcion});
+            }
+            return RegistrosCreados;
+        }
+
+        public static int DeleteById(int id){
+            int RegistrosEliminados = 0;
+            String query = "DELETE * FROM Pizzas WHERE Id = @id";
+
+            using (SqlConnection db = ){
+                RegistrosEliminados = db.Execute(query, new{Id = id});
+            }
+            return RegistrosEliminados;
+        }
+
+        public static int Update (Pizza pizza){
+            String query = "UPDATE Pizza SET (Nombre = @nombre, LibreGluten = @LibreGluten, Importe = @Importe, Descripcion = @Descripcion)";
+            int RegistrosActualizados = 0;
+
+            using (SqlConnection db = ){
+                RegistrosActualizados = db.Execute(query, new{nombre=pizza.nombre,libreGluten=pizza.LibreGluten,Importe=pizza.Importe,Descripcion=pizza.Descripcion});
+            }
+            return RegistrosActualizados;
+        }
+
+    }
+}
